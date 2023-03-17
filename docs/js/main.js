ee253e6d3f37a9f3e9ea0a1ebf7930df87d54f54
georapbox <georapbox@gmail.com>
2023-03-17 13:32:31 +0200

    Update demo

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,47 +1,8 @@
 const isLocalhost = window.location.href.includes('127.0.0.1') || window.location.href.includes('localhost');
 const componentUrl = isLocalhost ? '../../dist/a-tab-group.js' : '../lib/a-tab-group.js';
 const form = document.querySelector('form');
-const addTabButton = document.getElementById('add-tab-button');
-const removeTabButton = document.getElementById('remove-tab-button');
 const tabGroupEl = document.querySelector('a-tab-group');
 const consoleEl = document.getElementById('console');
-
-function randomText() {
-  const texts = [
-    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse qui at soluta unde doloremque laboriosam atque, similique animi! Recusandae libero fugit ad consequatur facilis praesentium maxime commodi quia quidem nostrum?',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate labore aut, exercitationem veniam non accusantium est iure quas suscipit ratione omnis laborum officiis voluptates tempore magnam dolor iste quia nihil.',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore ipsam suscipit dicta odio fugiat libero voluptates rerum ea doloremque quia dignissimos, quasi mollitia atque saepe. Quae sed accusantium accusamus atque!',
-    'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus animi quod dolore, consequatur tenetur provident enim maiores eveniet ipsa dolorum est. Saepe nam officiis modi, illo nostrum quibusdam autem eligendi.',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut minus nostrum necessitatibus magni unde delectus commodi labore facere consectetur. Doloremque labore iure fugiat omnis nam recusandae, minus provident temporibus sed.',
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi consectetur nulla, vero doloremque nam autem fugiat praesentium deleniti officiis reprehenderit harum aliquam iure impedit similique saepe nemo. Eligendi, nobis porro.',
-    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia facere ipsum corrupti fugiat repudiandae rerum vel explicabo! Voluptate corrupti voluptatibus consequuntur neque. Provident, quos illum! Ratione dolorem architecto totam quo.',
-    'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est iste, odio quasi dolorum neque aperiam, at suscipit blanditiis earum id qui et dolorem aut enim possimus. Beatae incidunt in culpa.'
-  ];
-
-  return texts[Math.floor(Math.random() * texts.length)];
-}
-
-function createTab(id) {
-  const tab = document.createElement('a-tab');
-  tab.id = `tab-${id}`;
-  tab.setAttribute('slot', 'tab');
-  tab.textContent = `Tab ${id}`;
-  tabGroupEl.appendChild(tab);
-
-  const panel = document.createElement('a-tab-panel');
-  panel.id = `tab-panel-${id}`;
-  panel.setAttribute('slot', 'panel');
-  panel.innerHTML = `<h3>Tab panel ${id}</h3><p>${randomText()}</p>`;
-  tabGroupEl.appendChild(panel);
-}
-
-function removeTab(id) {
-  const tab = document.getElementById(`tab-${id}`);
-  const panel = document.getElementById(`tab-panel-${id}`);
-
-  tab && tab.remove();
-  panel && panel.remove();
-}
 
 import(componentUrl).then(() => {
   form.addEventListener('input', evt => {
@@ -62,19 +23,12 @@ import(componentUrl).then(() => {
     }
   });
 
-  addTabButton.addEventListener('click', () => {
-    const totalTabs = document.querySelectorAll('a-tab').length;
-    createTab(totalTabs + 1);
+  document.addEventListener('a-tab-group:tab-change', evt => {
+    consoleEl.innerHTML = `a-tab-group:tab-change -> ${JSON.stringify(evt.detail)}`;
   });
 
-  removeTabButton.addEventListener('click', () => {
-    const allTabs = document.querySelectorAll('a-tab');
-
-    removeTab(allTabs.length);
-  });
-
-  document.addEventListener('a-tab-group:change', evt => {
-    consoleEl.innerHTML = `a-tab-group:change -> ${JSON.stringify(evt.detail)}`;
+  document.addEventListener('a-tab-group:tab-close', evt => {
+    consoleEl.innerHTML = `a-tab-group:tab-close -> ${JSON.stringify(evt.detail)}`;
   });
 }).catch(err => {
   console.error(err);
